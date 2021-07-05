4459c23bc6ee8b24c86e1d8c70a7ee32003a3788
ozgeonec <ozgenuronec@gmail.com>
2021-07-05 09:51:13 +0300

    cont.

--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -1,8 +1,25 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import _ from 'lodash';
 
 
 const PokemonList = () => {
-    return(
+    const dispatch = useDispatch();
+    const pokeList = useSelector(state => state.Po);
+
+    const ShowData = () => {
+        if (_.isEmpty(pokeList.data)) {
+            return <p>have data</p>
+        }
+        if (pokeList.loading) {
+            return <p>Loading...</p>
+        }
+        if (pokeList.errorMsg !== '') {
+            return <p>{pokeList.errorMsg}</p>
+        }
+    }
+
+    return (
         <div>
             Pokemon List
         </div>
--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,8 +1,9 @@
 import {combineReducers} from "redux";
+import PokemonListReducer from "./PokemonListReducer";
 
 
 const RootReducer = combineReducers({
-
+    PokemonList: PokemonListReducer
 });
 
 export default RootReducer;
